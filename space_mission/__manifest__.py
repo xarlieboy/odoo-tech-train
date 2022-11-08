098774739b2adc902855ca846a402035e5903cb3
CBoy <117315803+xarlieboy@users.noreply.github.com>
2022-11-07 20:21:58 -0400

    Initialize 15.0-space-mission-day01

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Space Mission',
+    'version': '0.1',
+    'summary': 'Odoo Application to control its Space Mission',
+    'description': """
+        Space Mission Application to Manage Space Control:
+            -
+            -
+            -
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    
+    'assets': {},
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+}
+
